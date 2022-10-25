a38e4f67378daf5f8f68cb5e5efec86743a016e7
Ria Das <riadas@node10-ccncluster.stanford.edu>
2022-10-25 11:19:13 -0700

    hypothesis generation data

---
+++ src/rational_rules/generative_model/bulk_hyp_generation_data_creation.sh
@@ -0,0 +1,9 @@
+#!.bin/bash
+
+for entry in "$1"/*
+do
+  echo "$entry"
+  python ../../../generative_model/create_existing_data_to_hyp_generation.py $entry none
+  python ../../../generative_model/create_existing_data_to_hyp_generation.py $entry missing-and-or
+  python ../../../generative_model/create_existing_data_to_hyp_generation.py $entry all
+done
